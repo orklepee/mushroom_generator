d2e8ad587cf0cc228a22b2e310d20497e8eef4e5
orklepee <svierling@keystone.edu>
2021-11-17 13:51:32 -0500

    Setup basic functions in files

--- src/scene/MenuScene.h
+++ src/scene/MenuScene.h
@@ -1,2 +1,12 @@
 #pragma once
 // Handle main menu
+
+#include "Scene.h"
+
+class MenuScene : public Scene
+{
+public:
+	void OnCreate() override;
+	void OnDestroy() override;
+	void OnUpdate(float dt) override;
+};
--- src/scene/Scene.h
+++ src/scene/Scene.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "../Game.h"
+
+class Scene
+{
+	// This allows Game class to access private members of Scene
+	// This does not mean that Scene has access to Game, though.
+	friend class Game;
+public:
+	virtual void OnCreate() = 0;
+	virtual void OnDestroy() = 0;
+	virtual void OnUpdate(float dt) = 0;
+};
+//using ScenePtr = std::shared_ptr<Scene>;
