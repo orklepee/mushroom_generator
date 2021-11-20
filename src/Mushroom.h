b24a7c38f2a0e840055bff13aa013d97ac3628d4
orklepee <svierling@keystone.edu>
2021-11-19 21:31:15 -0500

    Adding mushroom data files

---
+++ src/Mushroom.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <set>
+
+#include "../src/lib/olcPixelGameEngine.h"
+
+enum class MushSpecies
+{
+	Unassigned,
+	Bolete,
+	Chanterelle,
+	Roundhead
+};
+
+// Struct that handles basic Mushroom characteristics
+// Accessed via Mushroom
+struct MushBio
+{
+	MushSpecies Species;
+	int mushCapSize;
+	int mushStemLength;
+
+	// Declaring default MushSpecies to be unassigned
+	MushBio(MushSpecies species = MushSpecies::Unassigned)
+		:Species(species)
+	{
+	}
+
+};
+
+/* Mushroom class handles everything relating to a single instance of a mushroom:
+*		- Cap size, stem length, species
+*		- Sprite image
+*		- Chances of spawning
+*		- Sound when selecting
+*/
+class Mushroom
+{
+private:
+	std::set<MushBio> MushroomBiography;
+
+public:
+	MushBio GetMushSpecies
+};
